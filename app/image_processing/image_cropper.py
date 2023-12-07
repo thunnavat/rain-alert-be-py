26d1ed61e4e04a2b03e6be10eff8de1fe0aa7f87
thunnavat <thunnavat.c@mail.kmutt.ac.th>
2023-12-07 20:04:24 +0700

    refactor: change def crop_polygon

--- app/image_processing/image_cropper.py
+++ app/image_processing/image_cropper.py
@@ -9,35 +9,29 @@ class ImageCropper:
     def _decode_image(self):
         nparr = np.frombuffer(self.image_buffer, np.uint8)
         return cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-
+    
     def crop_polygon(self, polygon_vertices):
         # Create an empty mask with the same size as the image
-        mask = np.zeros(self.image.shape[:2], dtype=np.uint8)
+        mask = np.zeros_like(self.image)
 
         # Fill the mask with white pixels inside the polygon
-        cv2.fillPoly(mask, [polygon_vertices], 255)
+        cv2.fillPoly(mask, [polygon_vertices], (255, 255, 255))
+
+        # Bitwise AND operation between the mask and the original image
+        result = cv2.bitwise_and(self.image, mask)
 
         # Find the bounding box of the polygon
         x, y, w, h = cv2.boundingRect(polygon_vertices)
 
-        # Create an RGBA image with a transparent background
-        rgba_image = np.zeros((h, w, 4), dtype=np.uint8)
-        rgba_image[:, :, :3] = self.image[y:y+h, x:x+w]  # Copy the RGB channels
-
-        # Ensure the mask has the correct shape
-        mask_roi = mask[y:y+h, x:x+w]
-
-        # Set alpha channel based on the mask
-        rgba_image[:, :, 3] = (mask_roi > 0).astype(np.uint8) * 255
+        # Crop the region of interest from the result image
+        cropped_image = result[y:y+h, x:x+w]
 
         # Encode the cropped image as a byte buffer
-        success, buffer = cv2.imencode('.jpg', rgba_image)
+        success, buffer = cv2.imencode('.jpg', cropped_image)
 
         if success:
             print("Image fetched and stored in buffer successfully")
-            buffer.tobytes()
+            return buffer.tobytes()
         else:
             print("Failed to encode image to bytes")
             return None
-
-
